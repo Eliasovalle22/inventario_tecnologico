f6157d23c955c72a64234bd462959d148b8a6414
Ramón Elías Montesino Ovalle <remontesinoo22@ul.edu.co>
2026-02-27 13:54:26 -0500

    Update mejoras en el formulario de activos

--- inventario/forms.py
+++ inventario/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Activo
-from catalogos.models import Categoria, Marca, Ubicacion, Estado
+from catalogos.models import Categoria, Marca, Ubicacion, Estado, TipoActivo
 from django.contrib.auth.models import User
 
 class ActivoForm(forms.ModelForm):
@@ -19,19 +19,22 @@ class ActivoForm(forms.ModelForm):
         # Personalizar los campos
         self.fields['codigo'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Ej: ACT-001'})
         self.fields['codigo'].required = True
-        self.fields['tipo'].widget.attrs.update({'class': 'form-select'})
+        self.fields['tipo'].queryset = TipoActivo.objects.all().order_by('nombre')
+        self.fields['tipo'].widget.attrs.update({'class': 'form-select select2-tipo'})
         self.fields['modelo'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Modelo del equipo'})
         self.fields['valor_compra'].widget.attrs.update({'class': 'form-control', 'placeholder': '0.00'})
         self.fields['garantia_meses'].widget.attrs.update({'class': 'form-control', 'min': 0})
+        if not self.instance.pk:
+            self.fields['garantia_meses'].initial = 0
         self.fields['proveedor'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Nombre del proveedor'})
         self.fields['factura'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Número de factura'})
         
         # Para campos ForeignKey, asegurar que tengan las opciones correctas
         self.fields['categoria'].queryset = Categoria.objects.all().order_by('nombre')
-        self.fields['categoria'].widget.attrs.update({'class': 'form-select'})
+        self.fields['categoria'].widget.attrs.update({'class': 'form-select select2-categoria'})
         
         self.fields['marca'].queryset = Marca.objects.all().order_by('nombre')
-        self.fields['marca'].widget.attrs.update({'class': 'form-select'})
+        self.fields['marca'].widget.attrs.update({'class': 'form-select select2-marca'})
         
         self.fields['estado'].queryset = Estado.objects.all().order_by('nombre')
         self.fields['estado'].widget.attrs.update({'class': 'form-select'})
@@ -56,9 +59,10 @@ class ActivoForm(forms.ModelForm):
             except Ubicacion.DoesNotExist:
                 pass
         
-        self.fields['responsable'].queryset = User.objects.filter(is_active=True).order_by('username')
-        self.fields['responsable'].widget.attrs.update({'class': 'form-select'})
+        self.fields['responsable'].queryset = User.objects.filter(is_active=True).order_by('first_name', 'last_name')
+        self.fields['responsable'].widget.attrs.update({'class': 'form-select select2-responsable'})
         self.fields['responsable'].required = False
+        self.fields['responsable'].label_from_instance = lambda obj: f"{obj.first_name} {obj.last_name}".strip() or obj.username
         
         # Hacer algunos campos opcionales
         self.fields['valor_compra'].required = False
